e7e4482e59ce590515c2946326a03f3dcf8b91c9
Klaster28 <89096914521@mail.ru>
2022-10-27 23:26:06 +0300

    Коммит первый

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+
 public class Main {
-    public static void main(String[] args) {
+
+        public static void main(String[] args) {
+            Book masters = new Book ("Masters", 1983);
+            System.out.println("masters = " + masters.getName ());
+            masters.setYear(1986);
+            System.out.println("masters.getYear() = " + masters.getYear());
 
 
+
+        }
     }
-}
+
